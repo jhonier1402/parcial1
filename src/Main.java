931b7b55d545b498e600e1301084241bcced8513
Jhonier Sanchez <jhdsanchezg@udistrital.edu.co>
2025-05-13 21:33:27 -0500

    registro

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
+
+    // Paso 2: Declaramos arreglos globales para usuarios
+    static final int MAX_USUARIOS = 50; // Máximo número de usuarios
+
+    static String[] correos = new String[MAX_USUARIOS];     // Arreglo para guardar correos
+    static String[] contraseñas = new String[MAX_USUARIOS]; // Arreglo para guardar contraseñas
+    static int totalUsuarios = 0; // Lleva la cuenta de cuántos usuarios se han registrado
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // Creamos el lector de datos desde teclado
         boolean salir = false; // Variable para controlar la salida del programa
@@ -29,7 +37,35 @@ public class Main {
         }
         scanner.close();
     }
-}
+    public static void registrarUsuario(Scanner scanner) {
+        System.out.println("Ingrese su correo: ");//pedimos su correo
+        String correo = scanner.nextLine();
+
+        int i = 0;
+        boolean yaExiste = false;
+        while (i < totalUsuarios) {
+            if (correo.equals(correos[i])) {
+                yaExiste = true;
+                break;
+            }
+            i++;
+        }
+        if (yaExiste) {
+            System.out.println("El correo ya existe");
+            return;
+        }
+        System.out.println("cree una contraseña");
+        String contraseña = scanner.nextLine();
+
+        correos[totalUsuarios] = correo;
+        contraseñas[totalUsuarios] = contraseña;
+        totalUsuarios++;
+
+        System.out.println("Usuario registrado correctamente.");
+    }
+
+    }
+
 
 
 
